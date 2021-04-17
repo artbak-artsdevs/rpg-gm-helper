26ace550cc1ab9b641f164aa42c50150607045e3
artbak-artsdevs <abak@artsdevs.com>
2021-04-18 00:20:21 +0200

    added Character model with simple HTML on index

--- rpg_helper/characters_app/models.py
+++ rpg_helper/characters_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class Character(models.Model):
+
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    nickname = models.CharField(max_length=40, null=True, blank=True)
+    middle_name = models.CharField(max_length=40, null=True, blank=True)
+    surname = models.CharField(max_length=40, null=True, blank=True)
+    age = models.SmallIntegerField(null=True, blank=True)
+    alive = models.BinaryField()
+    race = models.CharField(null=True, max_length=50, blank = True)
+    main_story = models.TextField(null=True, blank = True)
+    notes = models.TextField(null=True, blank=True)
+    #TODO: add a campain model and campain variable to that model! OR stay with just relational db
+    #campain = models.ForeignKey()
+    created_date = models.DateTimeField(default=timezone.now)
+    #TODO: add proffession. It will be a field from campain modelor separated model.
+
+
--- rpg_helper/characters_app/views.py
+++ rpg_helper/characters_app/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from .models import Character
+from .templates import *
+
 
 # Create your views here.
 
 def main_page(request):
 
-    return HttpResponse('HEELOOO')
+    characters = Character.objects.all()
+
+    context = {'chars': characters}
+
+    return render(request, 'index.html', context=context )
